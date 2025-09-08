ae9004bc4860b9dc5891aabe901aab73bed26d4a
Sasha Marfin <46836780+SashaKeks@users.noreply.github.com>
2025-09-08 10:31:50 +0300

    write add DI with getIt init

--- lib/core/depends/main_depends.dart
+++ lib/core/depends/main_depends.dart
@@ -1,11 +1,18 @@
-
 import 'package:get_it/get_it.dart';
+import 'package:quiz_application/quiz/data/datasource/quiz_local_datasource/quiz_datasource_interface.dart';
+import 'package:quiz_application/quiz/data/datasource/quiz_local_datasource/quiz_local_datasource.dart';
+import 'package:quiz_application/quiz/data/service/quiz_service.dart';
+import 'package:quiz_application/quiz/domain/repository/quiz_repository.dart';
+import 'package:quiz_application/quiz/domain/usecase/get_quiz_usecase.dart';
 
 GetIt _getIt = GetIt.instance;
 
 class MainDepends {
-
   static void init() {
-    //TODO: Register dependencies here
+    _getIt.registerLazySingleton<QuizDatasourceInterface>(
+      () => QuizLocalDatasource(),
+    );
+    _getIt.registerLazySingleton<QuizRepository>(() => QuizService(_getIt()));
+    _getIt.registerFactory<GetQuizUseCase>(() => GetQuizUseCase(_getIt()));
   }
 }
