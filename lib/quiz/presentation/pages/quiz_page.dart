150be431e1a01b25cc36337260502f2477789f94
Sasha Marfin <46836780+SashaKeks@users.noreply.github.com>
2025-09-08 10:31:27 +0300

    write: add bottom navigation bar for switch between quiz and history

---
+++ lib/quiz/presentation/pages/quiz_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_application/quiz/presentation/widgets/card_stack_animated.dart';
+import 'package:quiz_application/quiz/presentation/widgets/quiz_history.dart';
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  int index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Quiz App')),
+      body: IndexedStack(
+        index: index,
+        children: const [CardStackAnimated(), QuizHistory()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.quiz), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
