fd36a61b6167d42c22f1dbc8c60b395c7ead47d1
Sasha Marfin <46836780+SashaKeks@users.noreply.github.com>
2025-09-08 09:21:42 +0300

    write: add event for next question start and stop animations

--- lib/quiz/presentation/bloc/quiz_event.dart
+++ lib/quiz/presentation/bloc/quiz_event.dart
@@ -2,14 +2,13 @@ class QuizEvent {}
 
 class QuizStarted extends QuizEvent {}
 
+class QuizNextQuestionStart extends QuizEvent {}
+
+class QuizNextQuestionEnd extends QuizEvent {}
+
 class QuizAnswered extends QuizEvent {
   final String questionId;
   final String selectedOptionId;
 
-  QuizAnswered({
-    required this.questionId,
-    required this.selectedOptionId,
-  });
+  QuizAnswered({required this.questionId, required this.selectedOptionId});
 }
-
-
