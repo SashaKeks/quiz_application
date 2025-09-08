ce073da89de54840876bf0734fad7e8badeb82a1
Sasha Marfin <46836780+SashaKeks@users.noreply.github.com>
2025-09-08 10:28:58 +0300

    write: quiz local datasource

---
+++ lib/quiz/data/datasource/quiz_local_datasource/quiz_local_datasource.dart
@@ -0,0 +1,53 @@
+import 'package:quiz_application/quiz/data/datasource/quiz_local_datasource/quiz_datasource_interface.dart';
+import 'package:quiz_application/quiz/data/model/option_model.dart';
+import 'package:quiz_application/quiz/data/model/question_model.dart';
+import 'package:quiz_application/quiz/data/model/quiz_model.dart';
+
+class QuizLocalDatasource implements QuizDatasourceInterface {
+  @override
+  Future<QuizModel> getQuiz() {
+    return Future.value(
+      QuizModel(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        name: "Flutter Quiz",
+        questions: [
+          QuestionModel(
+            id: "1",
+            questionText: "What is Flutter mainly used for?",
+            options: [
+              OptionModel(
+                id: "1.1",
+                optionText: "Cross-platform UI development",
+                isCorrect: true,
+              ),
+              OptionModel(id: "1.2", optionText: "Backend services"),
+              OptionModel(id: "1.3", optionText: "Database management"),
+            ],
+          ),
+          QuestionModel(
+            id: "2",
+            questionText: "Which language is Flutter written in?",
+            options: [
+              OptionModel(id: "2.1", optionText: "Dart", isCorrect: true),
+              OptionModel(id: "2.2", optionText: "Java"),
+              OptionModel(id: "2.3", optionText: "Swift"),
+            ],
+          ),
+          QuestionModel(
+            id: "3",
+            questionText: "What command creates a new project?",
+            options: [
+              OptionModel(
+                id: "3.1",
+                optionText: "flutter start",
+                isCorrect: true,
+              ),
+              OptionModel(id: "3.2", optionText: "flutter create"),
+              OptionModel(id: "3.3", optionText: "flutter init"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
