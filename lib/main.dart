11772ef2811ee81baf2e3c882438a6d02d9f33e8
Sasha Marfin <46836780+SashaKeks@users.noreply.github.com>
2025-09-08 10:33:06 +0300

    write: add bloc to screen for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:quiz_application/core/depends/main_depends.dart';
+import 'package:quiz_application/quiz/domain/usecase/get_quiz_usecase.dart';
+import 'package:quiz_application/quiz/presentation/bloc/quiz/quiz_bloc.dart';
+import 'package:quiz_application/quiz/presentation/bloc/quiz/quiz_event.dart';
+import 'package:quiz_application/quiz/presentation/pages/quiz_page.dart';
 
 void main() {
   MainDepends.init();
@@ -8,17 +14,40 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Quiz',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          backgroundColor: Colors.blueAccent,
+        ),
         useMaterial3: true,
       ),
-      home: const SizedBox(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create:
+                (_) =>
+                    QuizBloc(GetIt.instance<GetQuizUseCase>())
+                      ..add(QuizStarted()),
+          ),
+        ],
+        child: const QuizPage(),
+      ),
     );
   }
 }
