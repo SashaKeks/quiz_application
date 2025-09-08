3bfb29f7addb9437793895aad35c0947518bc4fe
Sasha Marfin <46836780+SashaKeks@users.noreply.github.com>
2025-09-08 11:40:37 +0300

    rewrite: change history data entity

--- lib/quiz/domain/entity/history_data_entity.dart
+++ lib/quiz/domain/entity/history_data_entity.dart
@@ -1,6 +1,15 @@
 class HistoryDataEntity {
   final String quizId;
+  final String quizTitle;
   final int score;
+  final int total;
+  final int timestamp;
 
-  HistoryDataEntity({required this.quizId, required this.score});
+  HistoryDataEntity({
+    required this.quizId,
+    required this.quizTitle,
+    required this.score,
+    required this.total,
+    required this.timestamp,
+  });
 }
