894c7d0bae900df6424ab7edbcb6249f5870ff86
Sasha Marfin <46836780+SashaKeks@users.noreply.github.com>
2025-09-08 09:19:11 +0300

    write: add more params for quiz_bloc_state

--- lib/quiz/presentation/bloc/quiz_state.dart
+++ lib/quiz/presentation/bloc/quiz_state.dart
@@ -3,13 +3,40 @@ import 'package:quiz_application/quiz/domain/entity/quiz_entity.dart';
 
 class QuizState extends Equatable {
   final QuizEntity? quiz;
+  final int currentQuestionIndex;
+  final bool isAnimated;
+  final int correctAnswersCount;
+  final Map<String, String> selectedOptionIds;
 
-  const QuizState({this.quiz});
+  const QuizState({
+    this.quiz,
+    this.currentQuestionIndex = 0,
+    this.isAnimated = false,
+    this.correctAnswersCount = 0,
+    this.selectedOptionIds = const {},
+  });
 
-  QuizState copyWith({QuizEntity? Function()? quiz}) {
-    return QuizState(quiz: quiz != null ? quiz() : this.quiz);
+  QuizState copyWith({
+    QuizEntity? Function()? quiz,
+    int? currentQuestionIndex,
+    bool? isAnimated,
+    int? correctAnswersCount,
+    Map<String, String>? selectedOptionIds,
+  }) {
+    return QuizState(
+      quiz: quiz != null ? quiz() : this.quiz,
+      currentQuestionIndex: currentQuestionIndex ?? this.currentQuestionIndex,
+      isAnimated: isAnimated ?? this.isAnimated,
+      correctAnswersCount: correctAnswersCount ?? this.correctAnswersCount,
+      selectedOptionIds: selectedOptionIds ?? this.selectedOptionIds,
+    );
   }
 
   @override
-  List<Object?> get props => [quiz];
+  List<Object?> get props => [
+    quiz,
+    isAnimated,
+    correctAnswersCount,
+    currentQuestionIndex,
+  ];
 }
